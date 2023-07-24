8aee1f30c4121ed659b2dcf963e98d221a5e4788
Boopster <beckystill@hotmail.com>
2023-07-24 12:59:16 +0000

    Copilot third commit v2

--- comments.js
+++ comments.js
@@ -1,40 +1,71 @@
 // create web server for comments
 // 1. create web server
-// 2. create database connection
-// 3. create schema
-// 4. create model
-// 5. create router
-// 6. create route
-// 7. create view
+// 2. add comments
+// 3. get comments
+// 4. delete comments
+// 5. edit comments
 
 // 1. create web server
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const port = 3000;
+app.use(express.json());
+app.use(cors());
 
-const PORT = 3000;
+// 2. add comments
+// 3. get comments
+// 4. delete comments
+// 5. edit comments
+let comments = [
+  {
+    id: 0,
+    name: "홍길동",
+    content: "안녕하세요",
+  },
+  {
+    id: 1,
+    name: "홍길동",
+    content: "안녕하세요",
+  },
+  {
+    id: 2,
+    name: "홍길동",
+    content: "안녕하세요",
+  },
+];
 
-// 2. create database connection
-const mongoose = require('mongoose');
-const db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', function(){
-    console.log('Connected to mongod server');
+app.get("/comments", (req, res) => {
+  res.json(comments);
 });
 
-mongoose.connect('mongodb://localhost/mongodb_tutorial');
+app.post("/comments", (req, res) => {
+  console.log(req.body);
+  const { name, content } = req.body;
+  const id = comments.length;
+  comments.push({
+    id,
+    name,
+    content,
+  });
+  res.status(201).json({ id });
+});
 
-// 3. create schema
-const Comment = require('./models/comment');
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((comment) => comment.id !== Number(id));
+  res.status(204).send();
+});
 
-// 4. create model
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, content } = req.body;
+  const targetComment = comments.find((comment) => comment.id === Number(id));
+  targetComment.name = name;
+  targetComment.content = content;
+  res.status(201).json({ id });
+});
 
-// 5. create router
-const router = require('./routes')(app, Comment);
-
-// 6. create route
-const server = app.listen(PORT, function(){
-    console.log('Server running on port: ' + PORT);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
